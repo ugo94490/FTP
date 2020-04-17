a3f2e687056c67b3b20dc41f767d7031b41faf40
Ugo LeviCescutti <ugolc@localhost.localdomain>
2020-04-17 22:11:47 +0200

    [ADD] add RETR

--- include/ftp.h
+++ include/ftp.h
@@ -10,6 +10,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/select.h>
 #include <sys/time.h>
 #include <sys/wait.h>
--- main.c
+++ main.c
@@ -324,19 +324,70 @@ int noop(client_t *client)
     return (0);
 }
 
+int retr_connection(client_t *client)
+{
+    socklen_t lenght_socket;
+    pid_t pid = 0;
+    struct stat buffer;
+    FILE *stream;
+    char *res = NULL;
+
+    if (stat(client->command[1], &buffer) != 0) {
+        dprintf(client->fd, "451 Requested action aborted: local error in processing.\r\n");
+        return (0);
+    }
+    res = malloc(sizeof(char) * (buffer.st_size + 1));
+    stream = fopen(client->command[1], "r"); 
+    fread(res, buffer.st_size + 1, 1, stream);
+    res[buffer.st_size] = 0;
+    dprintf(client->fd, "150 File status okay; about to open data connection.\r\n");
+    pid = fork();
+    if (pid == 0) {
+        lenght_socket = sizeof(client->sock.my_addr);
+        client->sock.fd_client = accept(client->sock.fd, (struct sockaddr *) &client->sock.my_addr, &lenght_socket);
+        dprintf(client->sock.fd_client, "%s", res);
+        dprintf(client->fd, "226 Closing data connection.\r\n");
+        close(client->sock.fd);
+        close(client->sock.fd_client);
+        client->mode = -1;
+        exit(0);
+    }
+    free(res);
+    return (0);
+}
+
+int stror_connection(client_t *client)
+{
+    return (0);
+}
+
 int retr(client_t *client)
 {
-    if (client->log != 1)
+    if (client->log != 1) {
         dprintf(client->fd, "530 Not Connected.\r\n");
-    (void)client;
+        return (0);
+    }
+    if (client->mode == -1)
+        dprintf(client->fd, "425 Can't open data connection.\r\n");
+    else if (my_strlen_tab(client->command) != 2)
+        dprintf(client->fd, "501 Too many parameters for PASS.\r\n");
+    else
+        retr_connection(client);
     return (0);
 }
 
 int stor(client_t *client)
 {
-    if (client->log != 1)
+    if (client->log != 1) {
         dprintf(client->fd, "530 Not Connected.\r\n");
-    (void)client;
+        return (0);
+    }
+    if (client->mode == -1)
+        dprintf(client->fd, "425 Can't open data connection.\r\n");
+    else if (my_strlen_tab(client->command) != 2)
+        dprintf(client->fd, "501 Too many parameters for PASS.\r\n");
+    else
+        stor_connection(client);
     return (0);
 }
 
