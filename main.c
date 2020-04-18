bbd20153f57451d90e76d1eb04346a1baf876bac
Ugo LeviCescutti <ugolc@localhost.localdomain>
2020-04-18 12:48:21 +0200

    [FIX] Fix init server

--- main.c
+++ main.c
@@ -457,49 +457,62 @@ int check_client(client_t *client)
     return (0);
 }
 
+void list_data(client_t *client, char *res)
+{
+    socklen_t lenght_socket;
+
+    lenght_socket = sizeof(client->sock.my_addr);
+    client->sock.fd_client = accept(client->sock.fd, (struct sockaddr *)
+    &client->sock.my_addr, &lenght_socket);
+    dprintf(client->sock.fd_client, "%s", res);
+    dprintf(client->fd, "226 Closing data connection.\r\n");
+    close(client->sock.fd);
+    close(client->sock.fd_client);
+    client->mode = -1;
+}
+
+char *read_list(char *res, int link[2])
+{
+    int n = 0;
+    char tmp[4096];
+
+    while ((n = read(link[0], tmp, sizeof(tmp))) > 0) {
+        res = dupcat(res, tmp, n);
+        if (n < 4096)
+            break;
+    }
+    return (res);
+}
+
+void exec_list(int link[2], client_t *client)
+{
+    dup2(link[1], STDOUT_FILENO);
+    close(link[0]);
+    close(link[1]);
+    if (my_strlen_tab(client->command) == 1)
+        execl("/bin/ls", "ls", "-l", (char *)0);
+    if (my_strlen_tab(client->command) == 2)
+        execl("/bin/ls", "ls", "-l", client->command[1], (char *)0);
+}
+
 int list(client_t *client)
 {
     pid_t pid = 0;
     int link[2];
-    char tmp[4096];
-    int n = 0;
     char *res = NULL;
-    socklen_t lenght_socket;
 
-    if (check_client(client) == 84)
+    if (check_client(client) == 84 || pipe(link) == -1 || (pid = fork()) == -1)
         return (0);
-    if (pipe(link) == -1)
-        return (0);
-    if ((pid = fork()) == -1)
-        return (0);
-    if (pid == 0) {
-        dup2(link[1], STDOUT_FILENO);
-        close(link[0]);
-        close(link[1]);
-        if (my_strlen_tab(client->command) == 1)
-            execl("/bin/ls", "ls", "-l", (char *)0);
-        if (my_strlen_tab(client->command) == 2)
-            execl("/bin/ls", "ls", "-l", client->command[1], (char *)0);
-    } else {
-        while ((n = read(link[0], tmp, sizeof(tmp))) > 0) {
-            res = dupcat(res, tmp, n);
-            if (n < 4096)
-                break;
-        }
-        wait(NULL);
-    }
+    if (pid == 0)
+        exec_list(link, client);
+    else
+        res = read_list(res, link);
+    wait(NULL);
     dprintf(client->fd, "150 File status okay; about to open data");
     dprintf(client->fd, " connection.\r\n");
     pid = fork();
     if (pid == 0) {
-        lenght_socket = sizeof(client->sock.my_addr);
-        client->sock.fd_client = accept(client->sock.fd, (struct sockaddr *)
-        &client->sock.my_addr, &lenght_socket);
-        dprintf(client->sock.fd_client, "%s", res);
-        dprintf(client->fd, "226 Closing data connection.\r\n");
-        close(client->sock.fd);
-        close(client->sock.fd_client);
-        client->mode = -1;
+        list_data(client, res);
         exit(0);
     }
     free(res);
@@ -537,27 +550,18 @@ int init_server(int port)
 {
     int my_socket = 0;
     int option = 1;
-    struct sockaddr_in my_addr;
+    struct sockaddr_in my_addr = init_my_addr(port);
     socklen_t lenght_socket = sizeof(my_addr);
 
     my_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (my_socket == -1) {
-        perror("Socket");
+    if (my_socket == -1)
         return (0);
-    }
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_addr.s_addr = INADDR_ANY;
-    my_addr.sin_port = htons(port);
     lenght_socket = sizeof(my_addr);
     if (setsockopt(my_socket, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR),
-    (char *)&option, sizeof(option)) < 0) {
-        perror("Setsockopt");
+    (char *)&option, sizeof(option)) < 0)
         return (0);
-    }
-    if (bind(my_socket, (struct sockaddr *) &my_addr, lenght_socket) == -1) {
-        perror("Bind");
+    if (bind(my_socket, (struct sockaddr *) &my_addr, lenght_socket) == -1)
         return (0);
-    }
     if (listen(my_socket, 100) == -1)
         perror("Listen");
     return (my_socket);
