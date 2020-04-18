98e08c80977b06acce4df4baef6409d5f3afdedc
Ugo LeviCescutti <ugolc@localhost.localdomain>
2020-04-18 13:28:17 +0200

    [FIX] Fix too long function

--- main.c
+++ main.c
@@ -189,25 +189,19 @@ int pasv(client_t *client)
             close(client->sock.fd);
         }
         client->sock.fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (client->sock.fd == -1) {
-            perror("Socket");
+        if (client->sock.fd == -1)
             return (0);
-        }
         client->sock.my_addr.sin_family = AF_INET;
         client->sock.my_addr.sin_addr.s_addr = INADDR_ANY;
         port = rand() % 3000 + 1024;
         client->sock.my_addr.sin_port = htons(port);
         size = sizeof(client->sock.my_addr);
         if (setsockopt(client->sock.fd, SOL_SOCKET, (SO_REUSEPORT |
-        SO_REUSEADDR), (char *)&option, sizeof(option)) < 0) {
-            perror("Setsockopt");
+            SO_REUSEADDR), (char *)&option, sizeof(option)) < 0)
             return (0);
-        }
         if (bind(client->sock.fd, (struct sockaddr *) &client->sock.my_addr,
-        size) == -1) {
-            perror("Bind");
+            size) == -1)
             return (0);
-        }
         if (listen(client->sock.fd, 100) == -1)
             perror("Listen");
         client->mode = 0;
@@ -248,28 +242,20 @@ int port_check(client_t *client)
     int option = 1;
 
     client->sock.fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (client->sock.fd == -1) {
-        perror("Socket");
+    if (client->sock.fd == -1)
         return (0);
-    }
     client->sock.my_addr.sin_family = AF_INET;
     client->sock.my_addr.sin_addr.s_addr = INADDR_ANY;
     client->sock.my_addr.sin_port = htons(port);
     size = sizeof(client->sock.my_addr);
     if (setsockopt(client->sock.fd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR),
-    (char *)&option, sizeof(option)) < 0) {
-        perror("Setsockopt");
+        (char *)&option, sizeof(option)) < 0)
         return (0);
-    }
     if (bind(client->sock.fd, (struct sockaddr *) &client->sock.my_addr,
-    size) == -1) {
-        perror("Bind");
+        size) == -1)
         return (0);
-    }
-    if (listen(client->sock.fd, 100) == -1) {
-        perror("Listen");
+    if (listen(client->sock.fd, 100) == -1)
         return (0);
-    }
     client->mode = 1;
     return (0);
 }
@@ -311,19 +297,34 @@ int noop(client_t *client)
     return (0);
 }
 
-int retr_connection(client_t *client)
+void retr_exec(client_t *client, char *res)
 {
     socklen_t lenght_socket;
+    lenght_socket = sizeof(client->sock.my_addr);
+    client->sock.fd_client = accept(client->sock.fd, (struct sockaddr *)
+    &client->sock.my_addr, &lenght_socket);
+    dprintf(client->sock.fd_client, "%s", res);
+    dprintf(client->fd, "226 Closing data connection.\r\n");
+    close(client->sock.fd);
+    close(client->sock.fd_client);
+}
+
+int connection_abort(client_t *client)
+{
+    dprintf(client->fd, "451 Requested action aborted: local error in");
+    dprintf(client->fd, " processing.\r\n");
+    return (0);
+}
+
+int retr_connection(client_t *client)
+{
     pid_t pid = 0;
     struct stat buffer;
     FILE *stream;
     char *res = NULL;
 
-    if (stat(client->command[1], &buffer) != 0) {
-        dprintf(client->fd, "451 Requested action aborted: local error in");
-        dprintf(client->fd, " processing.\r\n");
-        return (0);
-    }
+    if (stat(client->command[1], &buffer) != 0)
+        return (connection_abort(client));
     res = malloc(sizeof(char) * (buffer.st_size + 1));
     stream = fopen(client->command[1], "r");
     fread(res, buffer.st_size + 1, 1, stream);
@@ -332,38 +333,42 @@ int retr_connection(client_t *client)
     dprintf(client->fd, " connection.\r\n");
     pid = fork();
     if (pid == 0) {
-        lenght_socket = sizeof(client->sock.my_addr);
-        client->sock.fd_client = accept(client->sock.fd, (struct sockaddr *)
-        &client->sock.my_addr, &lenght_socket);
-        dprintf(client->sock.fd_client, "%s", res);
-        dprintf(client->fd, "226 Closing data connection.\r\n");
-        close(client->sock.fd);
-        close(client->sock.fd_client);
-        client->mode = -1;
+        retr_exec(client, res);
         exit(0);
     }
     free(res);
     return (0);
 }
 
-int stor_connection(client_t *client)
+void stor_exec(client_t *client, struct stat buffer, char *name, char *content)
 {
-    struct stat buffer;
-    char *name = NULL;
+    socklen_t lenght_socket;
+    FILE *file = NULL;
+    FILE *stream = NULL;
+
+    lenght_socket = sizeof(client->sock.my_addr);
+    client->sock.fd_client = accept(client->sock.fd, (struct sockaddr *)
+    &client->sock.my_addr, &lenght_socket);
+    stream = fopen(client->command[1], "r");
+    content = malloc(sizeof(char) * (buffer.st_size + 1));
+    fread(content, buffer.st_size + 1, sizeof(char), stream);
+    fclose(stream);
+    content[buffer.st_size] = 0;
+    file = fopen(name, "w+");
+    fwrite(content, buffer.st_size, sizeof(char), file);
+    fclose(file);
+    dprintf(client->fd, "226 Closing data connection.\r\n");
+    close(client->sock.fd);
+    close(client->sock.fd_client);
+    client->mode = -1;
+}
+
+char *get_name(client_t *client, char *name)
+{
     int len = 0;
     int i = 0;
     int offset = 0;
-    socklen_t lenght_socket;
-    pid_t pid = 0;
-    FILE *file = NULL;
-    FILE *stream = NULL;
-    char *content = NULL;
 
-    if (stat(client->command[1], &buffer) != 0) {
-        dprintf(client->fd, "451 Requested action aborted: local error in ");
-        dprintf(client->fd, "processing.\r\n");
-        return (0);
-    }
     len = strlen(client->command[1]);
     for (; len != 0 && client->command[1][len] != '/'; len--, i++);
     name = malloc(sizeof(char) * (i + 1));
@@ -372,26 +377,27 @@ int stor_connection(client_t *client)
             offset = 1;
     name = strncpy(name, client->command[1] + len + offset, i);
     name[i] = '\0';
+    return (name);
+}
 
+int stor_connection(client_t *client)
+{
+    struct stat buffer;
+    char *name = NULL;
+    pid_t pid = 0;
+    char *content = NULL;
+
+    if (stat(client->command[1], &buffer) != 0) {
+        dprintf(client->fd, "451 Requested action aborted: local error in ");
+        dprintf(client->fd, "processing.\r\n");
+        return (0);
+    }
+    name = get_name(client, name);
     dprintf(client->fd, "150 File status okay; about to open data");
     dprintf(client->fd, " connection.\r\n");
     pid = fork();
     if (pid == 0) {
-        lenght_socket = sizeof(client->sock.my_addr);
-        client->sock.fd_client = accept(client->sock.fd, (struct sockaddr *)
-        &client->sock.my_addr, &lenght_socket);
-        stream = fopen(client->command[1], "r");
-        content = malloc(sizeof(char) * (buffer.st_size + 1));
-        fread(content, buffer.st_size + 1, sizeof(char), stream);
-        fclose(stream);
-        content[buffer.st_size] = 0;
-        file = fopen(name, "w+");
-        fwrite(content, buffer.st_size, sizeof(char), file);
-        fclose(file);
-        dprintf(client->fd, "226 Closing data connection.\r\n");
-        close(client->sock.fd);
-        close(client->sock.fd_client);
-        client->mode = -1;
+        stor_exec(client, buffer, name, content);
         exit(0);
     }
     free(content);
@@ -570,7 +576,7 @@ int init_server(int port)
         return (0);
     lenght_socket = sizeof(my_addr);
     if (setsockopt(my_socket, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR),
-    (char *)&option, sizeof(option)) < 0)
+        (char *)&option, sizeof(option)) < 0)
         return (0);
     if (bind(my_socket, (struct sockaddr *) &my_addr, lenght_socket) == -1)
         return (0);
